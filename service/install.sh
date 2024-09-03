5894861999babefe2f30985e65f3347834ae649b
Erfan Mahvash <erfan.mahvash@zoodfood.com>
2024-09-03 15:45:48 +0330

    add: multiple platform for bin download

--- service/install.sh
+++ service/install.sh
@@ -1,13 +1,46 @@
 #!/bin/bash
 set -e
 version=$1
-binUrl="https://github.com/erfan-272758/eifa-gateway-deploy/releases/download/v$version/server"
+binUrl=""
 serviceName="eifa-gateway.service"
 repoUrl="https://raw.githubusercontent.com/erfan-272758/eifa-gateway-deploy/main"
 
+# initArch discovers the architecture for this system.
+initArch() {
+  ARCH=$(uname -m)
+  case $ARCH in
+    armv5*) ARCH="armv5";;
+    armv6*) ARCH="armv6";;
+    armv7*) ARCH="arm";;
+    aarch64) ARCH="arm64";;
+    x86) ARCH="386";;
+    x86_64) ARCH="amd64";;
+    i686) ARCH="386";;
+    i386) ARCH="386";;
+  esac
+}
+
+# initOS discovers the operating system for this system.
+initOS() {
+  OS=$(echo `uname`|tr '[:upper:]' '[:lower:]')
+
+  case "$OS" in
+    # Minimalist GNU for Windows
+    mingw*|cygwin*) OS='windows';;
+  esac
+}
+
+initBinUrl(){
+    binUrl=https://github.com/erfan-272758/eifa-gateway-deploy/releases/download/v${version}/eifa-gateway_v${version}_${OS}_${ARCH}
+}
+
 # requirements
 requirements(){
-    echo "### Install Requirements ###"
+    initOS
+    initArch
+    initBinUrl
+    
+     echo "### Install Requirements ###"
     if ! wget --version 1>/dev/null 2>/dev/null
     then
         sudo apt update -y
